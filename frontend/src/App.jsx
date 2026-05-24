7036c1fc1908fd77f57b65684f4cf0ef1e8a068c
Giuseppe Lisi <giuseppe.lisi.main@gmail.com>
2026-05-24 12:11:49 +0200

    min

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,20 @@
+import { useEffect, useState } from "react";
 import VideoFeedComponent from "../components/VideoFeedComponent";
+import { useParametersStore } from "../store/useParametersStore";
+import * as Tone from "tone";
 
 export default function App() {
+  const { cutoff } = useParametersStore();
+
   return (
     <>
-      <VideoFeedComponent />
+      <div className="flex">
+        <VideoFeedComponent />
+        <div className="p-6">
+          <h3>Parameters</h3>
+          <p>Cutoff: {cutoff}</p>
+        </div>
+      </div>
     </>
   );
 }
