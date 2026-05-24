2fb0937ed6ee746bce3ba1daf7068902eb1d25ae
Giuseppe Lisi <giuseppe.lisi.main@gmail.com>
2026-05-24 17:41:59 +0200

    update: Added synth component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,14 @@
-import { useEffect, useState } from "react";
 import VideoFeedComponent from "../components/VideoFeedComponent";
-import { useParametersStore } from "../store/useParametersStore";
-import * as Tone from "tone";
+import SynthComponent from "../components/SynthComponent";
+import { useEffect, useState } from "react";
 
 export default function App() {
-  const { cutoff } = useParametersStore();
 
   return (
     <>
       <div className="flex">
         <VideoFeedComponent />
-        <div className="p-6">
-          <h3>Parameters</h3>
-          <p>Cutoff: {cutoff}</p>
-        </div>
+        <SynthComponent />
       </div>
     </>
   );
