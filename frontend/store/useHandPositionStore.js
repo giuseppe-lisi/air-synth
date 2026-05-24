39870ad1b9e9fe49a4a4e7b99976cb6cc8850fb6
Giuseppe Lisi <giuseppe.lisi.main@gmail.com>
2026-05-24 22:12:30 +0200

    update: minor changes

--- frontend/store/useHandPositionStore.js
+++ frontend/store/useHandPositionStore.js
@@ -1,12 +1,12 @@
 import { create } from "zustand";
 import { subscribeWithSelector } from "zustand/middleware";
 export const useHandPositionStore = create(
-	subscribeWithSelector((set, get) => ({
-		indexX: 0,
-		indexY: 0,
+    subscribeWithSelector((set, get) => ({
+        indexX: 0,
+        indexY: 0,
 
-		setIndexPosition: (x, y) => {
-			set({ indexX: x, indexY: y });
-		},
-	})),
+        setIndexPosition: (x, y) => {
+            set({ indexX: x, indexY: y });
+        },
+    })),
 );
