e747cd2d88fcf623fc7d5eca047925c7c8a8d107
Giuseppe Lisi <giuseppe.lisi.main@gmail.com>
2026-05-24 21:26:05 +0200

    landmark: added synth manipulation script

---
+++ frontend/utils/synth.js
@@ -0,0 +1,59 @@
+import * as Tone from "tone";
+
+export const createSynth = () => {
+    // create filter
+    const filter = new Tone.Filter({
+        type: "lowpass",
+        frequency: 1000,
+    }).toDestination();
+    // create 2 oscillators
+    const osc1 = new Tone.OmniOscillator({
+        volume: -12,
+        type: "sine",
+    }).connect(filter);
+    const osc2 = new Tone.OmniOscillator({
+        volume: -12,
+        type: "sine",
+    }).connect(filter);
+
+    function startPlaying() {
+        osc1.start();
+        osc2.start();
+    }
+
+    function stopPlaying() {
+        osc1.stop();
+        osc2.stop();
+    }
+
+    function setWaveform(osc, wave) {
+        osc == "osc1" ? (osc1.type = wave) : (osc2.type = wave);
+        console.log("waves:", osc1.type, osc2.type);
+    }
+
+    function setVolume(osc, value) {
+        osc == "osc1"
+            ? osc1.volume.rampTo(value, 0.1)
+            : osc2.volume.rampTo(value, 0.1);
+    }
+
+    function setCutoffFrequency(value) {
+        filter.frequency = value;
+    }
+
+    function toSpeakers() {
+        filter.toDestination();
+    }
+
+    return {
+        osc1,
+        osc2,
+        filter,
+        startPlaying,
+        stopPlaying,
+        setVolume,
+        setWaveform,
+        setCutoffFrequency,
+        toSpeakers
+    };
+};
