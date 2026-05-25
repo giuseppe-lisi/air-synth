03449ba0ae09d4a06928f6abcc2858e06a0bd2c0
Giuseppe Lisi <giuseppe.lisi.main@gmail.com>
2026-05-25 17:42:59 +0200

    update: renamed getWebcam to getWebcamUtil

---
+++ frontend/utils/getWebcamUtil.js
@@ -0,0 +1,18 @@
+// gets access to camera on page load
+const getWebcam = async () => {
+	try {
+		let stream = await navigator.mediaDevices.getUserMedia({
+			video: {
+				width: { ideal: 640 },
+				height: { ideal: 480 },
+				frameRate: { ideal: 30 },
+			},
+			audio: false,
+		});
+		return stream;
+	} catch (error) {
+		console.log("Could not activate webcam feed:", error.message);
+	}
+};
+
+export { getWebcam };
